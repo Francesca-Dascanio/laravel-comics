53850bdc265a37ac4694935a1590dbe6dda3149d
“Francesca <dascanio.francesca@gmail.com>
2023-03-09 16:52:53 +0100

    Struttura header completata

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,96 @@
 <header>
-    Header
+    <!-- Header-top -->
+    <div class="text-bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex justify-content-end">
+                        <div>
+                            DC Power VISA
+                        </div>
+                        <div class="ps-4">
+                            ADDITIONAL DC SITE &#9662;
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Header-bottom -->
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex justify-content-between align-items-center py-4">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC">
+                        </div>
+
+                        <!-- Nav -->
+                        <nav>
+                            <ul class="d-flex list-unstyled my-size">
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        CHARACTERS
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        COMICS
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        MOVIES
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                         TV
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        GAMES
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        COLLECTIBLES
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        VIDEOS
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        FANS
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        NEWS
+                                    </a>
+                                </li>
+                                <li class="ps-3">
+                                    <a href="" class="text-decoration-none">
+                                        SHOP
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <!-- Search -->
+                        <div class="search my-size text-right">
+                            <input type="search" id="site-search" name="search" placeholder="Search">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
