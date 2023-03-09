5b89b3114fbcab681607c92478195169f9877f3d
“Francesca <dascanio.francesca@gmail.com>
2023-03-09 17:09:42 +0100

    Prova inserimento main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,22 @@
 
 @section('content')
     <!-- Qui dentro codice del main -->
+    <div>
+        Ciao
+    </div>
+
+    @if(count($comics) > 0)
+    <ul>
+        @foreach($comics as $comic)
+            <li>
+                {{ comic[title] }}
+            </li>
+        @endforeach
+    </ul>
+    @else
+        <h3>
+            Non ci sono fumetti
+        </h3>
+    @endif
+
 @endsection
